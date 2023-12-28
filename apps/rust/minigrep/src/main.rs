f7095b25f7b0e1331fca61fa2a5278cb43496160
iamseki <chrisyuri_19@hotmail.com>
2023-12-28 17:33:27 -0300

    feat: sends error messages to stderr in minigrep program

--- apps/rust/minigrep/src/main.rs
+++ apps/rust/minigrep/src/main.rs
@@ -6,12 +6,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     if let Err(e) = _rust_minigrep::run(config) {
-        println!("Appication error: {e}");
+        eprintln!("Appication error: {e}");
         process::exit(1);
     }
 }
