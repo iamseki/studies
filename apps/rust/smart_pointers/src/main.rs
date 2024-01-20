9843290c4e21129ba5a8a0e7014214cd248e1751
iamseki <chrisyuri_19@hotmail.com>
2024-01-20 18:09:40 -0300

    feat: print the numbers of pointers to the allocation using Rc

--- apps/rust/smart_pointers/src/main.rs
+++ apps/rust/smart_pointers/src/main.rs
@@ -39,10 +39,13 @@ fn main() {
     println!("An example with Rc<T> reference_counting");
 
     let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("Count after creating a = {}", Rc::strong_count(&a));
     let b = Cons(3, Rc::clone(&a));
-    let c = Cons(4, Rc::clone(&a));
+    println!("Count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 
-    dbg!(a);
-    dbg!(b);
-    dbg!(c);
 }
